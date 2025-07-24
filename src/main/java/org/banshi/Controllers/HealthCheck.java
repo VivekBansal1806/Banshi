47e5eaf3754c18df81c5e234774cc727113f7dbd
VivekBansal <kumarv98133@gmail.com>
2025-07-24 13:17:10 +0530

    update HealthCheck

--- src/main/java/org/banshi/Controllers/HealthCheck.java
+++ src/main/java/org/banshi/Controllers/HealthCheck.java
@@ -1,13 +1,17 @@
 package org.banshi.Controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class HealthCheck {
 
     @GetMapping("/health-check")
-    public String healthCheck() {
-        return "OK";
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("UP");
     }
+
 }
