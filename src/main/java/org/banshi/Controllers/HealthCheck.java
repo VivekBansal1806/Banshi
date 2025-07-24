3d4387b10167d98cc3135ad59fa43fbe486c5f6a
VivekBansal <kumarv98133@gmail.com>
2025-07-24 23:27:15 +0530

    add more health-checks

--- src/main/java/org/banshi/Controllers/HealthCheck.java
+++ src/main/java/org/banshi/Controllers/HealthCheck.java
@@ -2,16 +2,19 @@ package org.banshi.Controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class HealthCheck {
 
     @GetMapping("/health-check")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("UP");
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<String> healthCheck2() {
+        return ResponseEntity.ok("UP2");
     }
 
 }
