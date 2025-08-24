19f0c700dce55b3c2821eb65f9887dc12dc2ecd8
VivekBansal <kumarv98133@gmail.com>
2025-08-25 04:44:23 +0530

    change utc to local

--- src/main/java/org/banshi/BanshiApplication.java
+++ src/main/java/org/banshi/BanshiApplication.java
@@ -3,10 +3,12 @@ package org.banshi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BanshiApplication {
-
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
         SpringApplication.run(BanshiApplication.class, args);
     }
 
